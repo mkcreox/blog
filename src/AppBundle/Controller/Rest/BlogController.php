f63a8b143a3e4c08dd054b372f4a1fea17f19644
Michal Kozderka <mk@creox.cz>
2017-10-05 09:01:58 +0200

    add rest endpoint

--- src/AppBundle/Controller/Rest/BlogController.php
+++ src/AppBundle/Controller/Rest/BlogController.php
@@ -2,27 +2,44 @@
 
 namespace AppBundle\Controller\Rest;
 
+use AppBundle\Entity\Post;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use FOS\RestBundle\View\View;
 
 class BlogController extends FOSRestController
 {
     /**
      * @Rest\Get("/rest/blog/list")
      */
-    public function listAction(Request $request)
+    public function listAction()
     {
         $restresult = $this->getDoctrine()->getRepository(Post::class)->findAll();
 
         if ($restresult === null) {
-            return new View("there are no users exist", Response::HTTP_NOT_FOUND);
+            return new View("there are no users", Response::HTTP_NOT_FOUND);
         }
 
-        return $restresult;
+        $view = $this->view($restresult);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("/rest/blog/show/{id}")
+     */
+    public function showAction($id)
+    {
+        $restresult = $this->getDoctrine()->getRepository(Post::class)->find($id);
+
+        if ($restresult === null) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+
+        $view = $this->view($restresult);
+        return $this->handleView($view);
     }
 }
